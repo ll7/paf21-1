fd35f40c2fcd6fe5e8683b68bb13d9baded97ce0
Marco Tröster <marco@troester-gmbh.de>
2021-11-09 18:06:13 +0100

    simplified parameter settings generalized roslaunch to launch custom launchfiles

--- docker/carla_ros_bridge/ros_entrypoint.sh
+++ docker/carla_ros_bridge/ros_entrypoint.sh
@@ -4,14 +4,8 @@ set -e
 # setup ros environment
 source "/opt/carla-ros-bridge/install/setup.bash"
 
-# override the CARLA simulator hostname
-sed -i "s/name='host' default=''/name='host' default='"$CARLA_SIM_HOST"'/g" \
-    /opt/carla-ros-bridge/install/share/carla_ros_bridge/launch/carla_ros_bridge.launch
-sed -i "s/name='host' default='localhost'/name='host' default='"$CARLA_SIM_HOST"'/g" \
-    /opt/carla-ros-bridge/install/share/carla_ros_bridge/launch/carla_ros_bridge_with_example_ego_vehicle.launch
-
 # wait for the CARLA simulator to launch
 sleep $CARLA_SIM_WAIT_SECS
 
 # launch the CARLA ROS bridge
-roslaunch carla_ros_bridge $@
+roslaunch $@
