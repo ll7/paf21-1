fbf26204bf8a83662121082d334498bac583de94
Marco Tröster <marco@troester-gmbh.de>
2021-12-01 00:06:13 +0100

    cleaned up ros bridge compose

--- components/carla_ros_bridge/ros_entrypoint.sh
+++ components/carla_ros_bridge/ros_entrypoint.sh
@@ -7,5 +7,5 @@ source "/opt/carla-ros-bridge/install/setup.bash"
 # wait for the CARLA simulator to launch
 sleep $CARLA_SIM_WAIT_SECS
 
-# launch the CARLA ROS bridge
+# launch the given ROS nodes from Docker CMD args
 roslaunch $@
