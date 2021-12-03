294045a0216a36fa298363a01f343c1696bcf87e
Marco Tröster <marco@troester-gmbh.de>
2021-12-03 03:35:14 +0100

    modified default behavior to car standing still

--- components/vehicle_controller/node/src/tests/test_vehicle_controller/test_vehicle_controller.py
+++ components/vehicle_controller/node/src/tests/test_vehicle_controller/test_vehicle_controller.py
@@ -1,6 +1,6 @@
 from vehicle_controller.driving import SimpleDrivingSignalConverter
 
-def test_should_drive_straight_forward():
+def test_should_stand_still():
     agent = SimpleDrivingSignalConverter()
     signal = agent.next_signal()
-    assert(signal.steering_angle == 0.0 and signal.speed > 0)
+    assert(signal.steering_angle == 0.0 and signal.speed == 0)
