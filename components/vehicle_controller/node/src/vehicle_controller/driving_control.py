5d7a154ae14cef5cb94c60c54af7051005b968d7
Marco Tröster <marco@troester-gmbh.de>
2021-12-07 22:26:16 +0100

    added driving controller tests added steering angle computation (wip)

--- components/vehicle_controller/node/src/vehicle_controller/driving_control.py
+++ components/vehicle_controller/node/src/vehicle_controller/driving_control.py
@@ -5,6 +5,7 @@ from math import dist as euclid_dist
 from datetime import datetime
 from typing import Tuple, List
 from dataclasses import dataclass, field
+import numpy as np
 
 @dataclass
 class DrivingSignal:
@@ -14,14 +15,18 @@ class DrivingSignal:
 
 @dataclass
 class SimpleDrivingController:
-    """A class for processing waypoints and
-    driving metadata into driving signals
-    regarding velocity and steering control"""
+    """A class for processing waypoints and driving metadata into
+    actionable driving signals regarding velocity and steering control"""
+
     route_waypoints: List[Tuple[float, float]] = field(default_factory=list)
     target_velocity_mps: float = 0.0
     actual_velocity_mps: float = 0.0
     vehicle_pos: Tuple[float, float] = None
     vehicle_pos_timestamp: datetime = None
+
+    # car length between axles and max steering angle of Tesla Model 3
+    car_length_in_m: float = 3.1
+    max_steering_angle: float = 0.5
 
     def update_route(self, waypoints: List[Tuple[float, float]]):
         """Update the route to be followed"""
@@ -49,18 +54,33 @@ class SimpleDrivingController:
         self.vehicle_pos_timestamp = new_timestamp
 
     def next_signal(self) -> DrivingSignal:
-        """Compute the next AckermannDrive signal to
-        follow the suggested ideal route"""
+        """Compute the next driving signal to make the
+        vehicle follow the suggested ideal route"""
 
+        # apply not overshooting max. steering angle here ...
         steering_angle = self._compute_steering_angle()
         return DrivingSignal(steering_angle, self.target_velocity_mps)
 
-    def _compute_steering_angle(self): # pylint: disable=no-self-use
-        # source: https://dingyan89.medium.com/three-methods-of-vehicle-lateral-control-pure-pursuit-stanley-and-mpc-db8cc1d32081 # pylint: disable=line-too-long
-        # source: http://www.shuffleai.blog/blog/Simple_Understanding_of_Kinematic_Bicycle_Model.html # pylint: disable=line-too-long
-        return 0.0
+    def _compute_steering_angle(self):
+        waypoints = self.route_waypoints
+        if len(waypoints) < 5:
+            return 0.0
+
+        curve_radius = SimpleDrivingController._compute_curvature_radius(
+            waypoints[0], waypoints[2], waypoints[4])
+        phi = np.arctan(self.car_length_in_m / curve_radius)
+        return phi
 
     # @staticmethod
-    # def _compute_turn_circle_radius(waypoints: List[Tuple[float, float]]):
-    #     near_points = filter(lambda wp: euclid_dist(waypoints[0], wp) <= 20, waypoints)
-        # interpolate the curve, e.g. with a taylor polynomial
+    # def euclid_dist(p1, p2):
+    #     # use standard function instead
+    #     return np.sqrt(pow(p1[0] - p2[0], 2) + pow(p1[1] - p2[1], 2))
+
+    @staticmethod
+    def _compute_curvature_radius(p_1, p_2, p_3):
+        triangle_area = ((p_2[0] - p_1[0]) * (p_3[1] - p_1[1]) - \
+                        (p_3[0] - p_1[0]) * (p_2[1] - p_1[1])) / 2.0
+        menger = (4 * triangle_area) / \
+                (euclid_dist(p_1, p_2) * euclid_dist(p_2, p_3) * euclid_dist(p_3, p_1))
+        radius = abs(1.0 / menger)
+        return radius
