11737945088d6dc96a5f52a78a7cbd9acef1b1f0
Johannes Stoljar <johannes.stoljar@uni-a.de>
2021-12-08 23:12:45 +0100

    refactor GlobalRoutePlanner and XODRConverter

--- components/global_planner/node/src/global_planner/global_route_planner.py
+++ components/global_planner/node/src/global_planner/global_route_planner.py
@@ -1,17 +1,23 @@
 import math
-
+import json
 import numpy as np
 import networkx as nx
-#from std_msgs.msg import String as StringMsg
-#from sensor_msgs.msg import NavSatFix as GpsMsg
+import os.path
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
-#import xodr_converter
+# for ROS connection
+# from std_msgs.msg import String as StringMsg
+# from sensor_msgs.msg import NavSatFix as GpsMsg
+# import xodr_converter
 
 
 class GlobalRoutePlanner:
+    """A global route planner based on map and hmi data."""
     def __init__(self, num_nodes):
+        """Initialize the global route planner."""
         self.num_nodes = num_nodes
+        # base filepath to the maps
+        self.filepath = r"../../../maps"
         # dict with lane-let ids and matrix pos
         self.nodes = {}
         # counter for number of nodes
@@ -20,82 +26,95 @@ class GlobalRoutePlanner:
         self.graph = np.zeros(shape=(num_nodes, num_nodes))
         self.graph_start_end = np.zeros(shape=(num_nodes+2, num_nodes+2))
         # initialize all distances with inf.
-        self.dist = [float("Inf")] * (num_nodes+2)
+        self.dist = np.ones(shape=(num_nodes+2,)) * np.inf
         # array for the parents to store shortest path tree
-        self.parent = [-1] * (num_nodes+2)
+        self.parent = np.ones(shape=(num_nodes+2,)).astype('int32') * (-1)
         # path
         self.path = []
-        #mapping
+        # mapping
         self.mapping = []
 
         self.world_radius = 6378137
         self.map_name = ''
         self.end_pos = (0.0, 0.0)
         self.start_pos = (0.0, 0.0)
+        self.update = False
         # TODO read from data
         self.road_width = 4.0
-        self.update = False
 
-    def set_matrix(self, matrix):
+    def set_matrix(self, matrix: np.ndarray):
         self.graph = matrix
 
-    def set_mapping(self, mapping):
+    def set_mapping(self, mapping: list):
         self.mapping = mapping
 
-    def add_node(self, node_id: str):
-        if len(self.nodes) < self.num_nodes:
-            # create new entry in dict
-            self.nodes[node_id] = self.matrix_pos
-            # add the matrix position up
-            self.matrix_pos += 1
-        else:
-            print('Reached maximum of nodes!')
+    def load_map_data(self):
+        """Load the data from the file with the map
+            name and set the mapping and the matrix."""
+        map_path = os.path.join(self.filepath, self.map_name, ".json")
+        if not os.path.isfile(map_path):
+            return FileNotFoundError
 
-    def get_pos(self, node_id):
-        return self.nodes[node_id] if node_id in self.nodes else -1
+        with open(map_path) as json_file:
+            data = json.load(json_file)
+            self.set_mapping(data['mapping'])
+            self.set_matrix(data['matrix'])
 
-    # TODO auslagern in XODR Converter / RL
-    def add_edge(self, start_id, target_id, weight):
-        # check if start_id and target_id ar in the dict
-        if start_id not in self.nodes:
-            self.add_node(start_id)
-        if target_id not in self.nodes:
-            self.add_node(target_id)
+    # def set_map_end(self, msg: StringMsg):
+    def set_map_end(self, msg):
+        """Update the current map name"""
+        if self.map_name != msg.map:
+            self.map_name = msg.map
+            self.load_map_data()
 
-        start_pos = self.get_pos(start_id)
-        target_pos = self.get_pos(target_id)
+        self.end_pos = msg.end_point
+        self.update = True
 
-        # set the weight in the graph
-        self.graph[start_pos, target_pos] = weight
-        # self.graph[target_pos, start_pos] = -1.0
+    # def set_gps(self, msg: GpsMsg):
+    def set_gps(self, msg):
+        """Update the GPS position of the vehicle."""
+        longitude = msg.longitude
+        latitude = msg.latitude
 
+        x = self.world_radius * math.sin(latitude) * math.cos(longitude)
+        y = self.world_radius * math.sin(latitude) * math.sin(longitude)
 
-    def min_distance(self, queue):
+        x2 = self.world_radius * longitude * math.cos(latitude)
+        y2 = self.world_radius * latitude
+
+        print(x, '  ', y)
+        print(x2, '  ', y2)
+        self.start_pos = (x, y)
+
+    def get_pos(self, node_id: str) -> int or KeyError:
+        """Get the position for the node id."""
+        if node_id not in self.num_nodes:
+            return KeyError
+
+        return self.nodes[node_id]
+
+    def _min_distance(self, queue: list) -> int or AttributeError:
+        """Calculate the index with the minimum distance."""
         # initialize min value and min_index as -1
         minimum = np.inf
-        min_index = 0
+        min_index = -1
 
         # from the dist array, pick one which has min value and is till in queue
-        for index in range(self.num_nodes):
-            if self.dist[index] < minimum and index in queue:
+        for index in queue:
+            if self.dist[index] < minimum:
                 # set the new minimum
                 minimum = self.dist[index]
                 # set new index
                 min_index = index
+
+        if min_index == -1:
+            return AttributeError
+
         # return the index
         return min_index
 
-    def min_distance2(self, queue, dist):
-        # initialize min value and min_index as -1
-        minimum = float("Inf")
-        min_index = -1
-        for i in range(len(dist)):
-            if dist[i] < minimum and i in queue:
-                minimum = dist[i]
-                min_index = i
-        return min_index
-
     def dijkstra(self, start_id: str):
+        """Implementation of the Dijkstra algorithm."""
         # distance of source to itself is 0
         start_pos = self.get_pos(start_id)
         self.dist[start_pos] = 0.0
@@ -106,73 +125,52 @@ class GlobalRoutePlanner:
         # find the shortest path for all nodes
         while queue:
             # pick the minimum dist node from the set of nodes
-            index_min = self.min_distance(queue)
+            index_min = self._min_distance(queue)
             # remove min element
             queue.remove(index_min)
 
             # update dist value and parent
-            for num in range(self.num_nodes):
+            for num in queue:
                 # update dist[i] if it is in queue, there is an edge from index_min to i,
-                if self.graph[index_min][num] and num in queue:
+                if self.graph[index_min][num]:
                     new_dist = self.dist[index_min] + self.graph[index_min][num]
                     if new_dist < self.dist[num]:
                         self.dist[num] = new_dist
                         self.parent[num] = index_min
-        # return self.dist, self.parent
 
-    def dijkstra2(self, src):
-        row = len(self.graph)
-        col = len(self.graph[0])
-        dist = [float("Inf")] * (row+2)
-        dist[src] = 0.0
-        queue = []
-        #Fill queue 0- x
-        for i in range(row+2):
-            queue.append(i)
-
-        while queue:
-            u = self.min_distance2(queue, dist)
-            if u == -1:
-                return
-            queue.remove(u)
-            for i in range(col):
-                if self.graph_start_end[u][i] and i in queue:
-                    if dist[u] + self.graph_start_end[u][i] < dist[i]:
-                        dist[i] = dist[u] + self.graph_start_end[u][i]
-                        self.parent[i] = u
-            self.dist = dist
-
-    def append_path(self, pos):
-        # Base Case : If j is source
-        if self.parent[pos] == -1:
-            self.path.append(pos)
-            return pos
-        self.append_path(self.parent[pos])
-
+    def _append_pos2path(self, pos: int) -> int:
+        """Append the position to the path"""
         self.path.append(pos)
+        # check if pos has a parent
+        if self.parent[pos] != -1:
+            # recursive call
+            self._append_pos2path(self.parent[pos])
+
         return pos
 
-    def calculate_route(self, target_id):
-        for num in range(1, self.num_nodes):
-            if num == target_id:
-                self.append_path(num)
+    def _append_id2path(self, target_id: str):
 
+        # get the position
+        pos = self.get_pos(target_id)
+        # append the position to the path
+        self._append_pos2path(pos)
 
-    def get_path_ids(self):
+    def get_path_ids(self) -> list:
         key_list = list(self.nodes.keys())
-        return [key_list[p] for p in self.path]
+        return [key_list[pos] for pos in self.path]
 
     def show_graph_with_labels(self):
+        # get all edges out of the graph with value greater 0
         edges = np.where(self.graph > 0)
         gr = nx.Graph()
+        # add all edges
         gr.add_edges_from(edges)
+        # draw the graph
         nx.draw(gr, node_size=500, labels=list(self.nodes.keys()), with_labels=True)
 
-    def find_nearest_road(self):
-        ids_Start = []
-        ids_End = []
-        point = self.start_pos
-        point2 = self.end_pos
+    def find_nearest_road(self) -> (list, list):
+        ids_start = []
+        ids_end = []
         self.graph_start_end = np.append(np.copy(self.graph), np.zeros((2, self.num_nodes)), axis=0)
         self.graph_start_end = np.append(self.graph_start_end, np.zeros((self.num_nodes+2, 2)), axis=1)
         print(self.graph_start_end.shape)
@@ -197,40 +195,39 @@ class GlobalRoutePlanner:
             ru_corner = (end_point[0] - offset_x, end_point[1] + offset_y)
 
             polygon = Polygon([ll_corner, lu_corner, ru_corner, rl_corner])
-            if polygon.contains(Point(point[0], point[1])):
-                ids_Start.append([self.mapping[i][0], self.mapping[i][1]])
+            if polygon.contains(Point(self.start_pos[0], self.start_pos[1])):
+                ids_start.append([self.mapping[i][0], self.mapping[i][1]])
                 # ToDo Gewichtung setzten zu start und ende (Abstand)
                 self.graph_start_end[i][self.num_nodes] = 10
                 self.graph_start_end[self.num_nodes][i] = 10
-                print('start road:', self.mapping[i][0] , ' i:', i)
+                print('start road:', self.mapping[i][0], ' i:', i)
 
-            if polygon.contains(Point(point2[0], point2[1])):
-                ids_End.append([self.mapping[i][0], self.mapping[i][1]])
+            if polygon.contains(Point(self.end_pos[0], self.end_pos[1])):
+                ids_end.append([self.mapping[i][0], self.mapping[i][1]])
                 self.graph_start_end[i][self.num_nodes+1] = 10
                 self.graph_start_end[self.num_nodes+1][i] = 10
                 print('end road:', self.mapping[i][0], ' i:', i)
-        return ids_Start, ids_End
+        return ids_start, ids_end
 
     def calculate_distance(self, point):
         pass
 
     def compute_route(self):
-        # 0. prüfen ob die map daten vorhanden sind
-        # nur neu laden falls neue map
+        # 0. check map data is available
+        # reload if new mapp
 
-        # 1. load and set map
-            # Done
+        # 1. load and set map -> Done
+
         # 2. start and endpoint
         self.start_pos = (20.0, 0.004)
         self.end_pos = (250.0, -0.004)
-            # 2.05 Finde Punkte
+        # 2.05 find points
         self.find_nearest_road()
-            # 2.1 StartPunkt in Matrix einfuegen --> n-2  /  gefundener punkt
+        # 2.1 insert start point to matrix --> n-2  /  found point
 
-            # 2.2 EndPunkt in Matrix einfuegen --> n-1  /  gefundener punkt
+        # 2.2 insert end point to matrix --> n-1  /  found point
 
         # from start and end point get id
-
 
         # 3. start dijkstra
         # ToDo
@@ -238,10 +235,10 @@ class GlobalRoutePlanner:
         print('start: ', self.graph_start_end[15][self.num_nodes])
         print('end: ', self.graph_start_end[6][self.num_nodes+1])
         print('end: ', self.graph_start_end[7][self.num_nodes+1])
-        self.dijkstra2(self.num_nodes)
+        self.dijkstra(self.num_nodes)
         print(self.dist)
         # TODO
-        self.calculate_route(6)
+        self._append_id2path('6')
         list_lanes = []
         list_waypoints = []
         print(self.path)
@@ -256,40 +253,3 @@ class GlobalRoutePlanner:
 
         # 5. output thr route
         pass
-
-    def load_map_data(self):
-        pass
-
-
-    #def set_map_end(self, msg: StringMsg):
-    def set_map_end(self, msg):
-        if self.map_name != msg.map:
-            self.map_name = msg.map
-            self.load_map_data()
-
-        self.end_pos = msg.end_point
-        self.update = True
-
-
-    def load_map_data(self):
-        #self.filepath
-        #self.map_name
-        pass
-
-    #def set_gps(self, msg: GpsMsg):
-    def set_gps(self, msg):
-        """Update the GPS position of the vehicle"""
-        longitude = msg.longitude
-        latitude = msg.latitude
-
-        x = self.world_radius * math.sin(latitude) * math.cos(longitude)
-        y = self.world_radius * math.sin(latitude) * math.sin(longitude)
-
-        x2 = self.world_radius * longitude * math.cos(latitude)
-        y2 = self.world_radius * latitude
-
-        print(x, '  ', y)
-        print(x2, '  ', y2)
-        self.start_pos = (x, y)
-
-
