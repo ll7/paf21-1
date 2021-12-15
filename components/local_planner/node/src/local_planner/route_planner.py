c6731dd55a6d7256c2d2bd2428175f9bc5f83c28
berghojo <berghoff.joshua@googlemail.com>
2021-12-15 03:51:46 +0100

    Lane Detection Working and Presentable Works with global Planner

--- components/local_planner/node/src/local_planner/route_planner.py
+++ components/local_planner/node/src/local_planner/route_planner.py
@@ -55,7 +55,7 @@ class RouteInfo(metaclass=SingletonMeta): # pylint: disable=too-many-locals
         """Update the global route to follow"""
         self.global_route = msg
         rospy.loginfo(self.global_route)
-        # self.global_route = None
+        #self.global_route = None
         if self.global_route is None:
             self.cached_local_route = []
         else:
@@ -82,7 +82,7 @@ class RouteInfo(metaclass=SingletonMeta): # pylint: disable=too-many-locals
         # self.cached_local_route = self.global_route[max(neighbour_ids[0], neighbour_ids[1]):]
         short_term_route = self.cached_local_route[:min(50, len(self.cached_local_route))]
 
-        turned_on = False
+        turned_on = True
         if images.semantic_image is not None and turned_on:
             image = images.semantic_image[:, :, :3] # cut off alpha channel
             highlighted_img, keep_lane, angle = self.lane_detection.detect_lanes(image)
@@ -99,10 +99,11 @@ class RouteInfo(metaclass=SingletonMeta): # pylint: disable=too-many-locals
                     cos = np.dot(route_vector, predicted_vector)/np.linalg.norm(
                         route_vector) / np.linalg.norm(predicted_vector)
                     off_set_angle = np.rad2deg(np.arccos(cos))
-                    if abs(off_set_angle) < 45:
+                    rospy.loginfo(f'off_set_angle:{off_set_angle}')
+                    if abs(off_set_angle) < 20:
                         short_term_route.insert(0, predicted_position)
-                        rospy.loginfo(f'step: {self.step_semantic}')
-                        rospy.loginfo(f'{short_term_route[0]} {self.vehicle_position}')
+                    rospy.loginfo(f'step: {self.step_semantic}')
+                    rospy.loginfo(f'next: {short_term_route[0]}')
                 else:
                     short_term_route.insert(0, predicted_position)
             if self.step_semantic % 10 == 0 and self.step_semantic < 10000:
