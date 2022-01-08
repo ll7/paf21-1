afe198817268bf7d5774d0dd94f516a638e6a66f
Johannes Stoljar <johannes.stoljar@uni-a.de>
2022-01-08 23:26:49 +0100

    Add plotting script for road sections

--- components/global_planner/node/src/global_planner/plot_waypoints.py
+++ components/global_planner/node/src/global_planner/plot_waypoints.py
@@ -16,6 +16,46 @@ def plot_waypoints(points: List[Tuple[float, float]]):
     plt.show()
 
 
+# def print_roads():
+#     _, ax = plt.subplots()
+#     for road in xodr_map.lane_lets:
+#         if road.road_id == 333:
+#             points = [geo.start_point for geo in road.geometries]
+#             xs = [-p[1] for p in points]
+#             ys = [p[0] for p in points]
+#             ax.scatter(xs, ys, c='red')
+#             n = range(0, len(xs))
+#             for i, txt in enumerate(n):
+#                 ax.annotate(txt, (xs[i], ys[i]))
+#         if road.road_id == 334:
+#             points = [geo.start_point for geo in road.geometries]
+#             xs = [-p[1] for p in points]
+#             ys = [p[0] for p in points]
+#             ax.scatter(xs, ys, c='green')
+#             n = range(0, len(xs))
+#             for i, txt in enumerate(n):
+#                 ax.annotate(txt, (xs[i], ys[i]))
+#         if road.road_id == 355:
+#             points = [geo.start_point for geo in road.geometries]
+#             points += [geo.end_point for geo in road.geometries]
+#             xs = [-p[1] for p in points]
+#             ys = [p[0] for p in points]
+#             ax.scatter(xs, ys, c='blue')
+#             n = range(0, len(xs))
+#             for i, txt in enumerate(n):
+#                 ax.annotate(txt, (xs[i], ys[i]))
+#
+#             points = [(150.99, -57.5)]
+#             xs = [-p[1] for p in points]
+#             ys = [p[0] for p in points]
+#             ax.scatter(xs, ys, c='yellow')
+#             n = range(0, len(xs))
+#             for i, txt in enumerate(n):
+#                 ax.annotate('P', (xs[i], ys[i]))
+#     plt.savefig('test.png')
+#     plt.show()
+
+
 if __name__ == '__main__':
     waypoints = [(396.6376037597656, -208.82986450195312), (394.3800048827421, -9.84999948643781),
                  (392.3810650632555, -8.970101400465818), (390.3990324152924, -1.272024735923396),
