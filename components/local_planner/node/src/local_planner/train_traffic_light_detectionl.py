5c2f8772eb202619e204d436c0bd69dfa662fe95
Johannes Stoljar <johannes.stoljar@uni-a.de>
2022-01-09 13:33:40 +0100

    Simplify Tiny ResNet and extract function of training class to static methods

--- components/local_planner/node/src/local_planner/train_traffic_light_detectionl.py
+++ components/local_planner/node/src/local_planner/train_traffic_light_detectionl.py
@@ -1,5 +1,4 @@
-import dataclasses
-from typing import List, Callable, Union, Iterable, Iterator
+from typing import List, Dict
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import numpy as np
@@ -23,7 +22,7 @@ class IdentityBlock(tf.keras.layers.Layer):
         self.conv3 = tf.keras.layers.Conv2D(filters=f_3, kernel_size=(1, 1), strides=(1, 1))
         self.bn3 = tf.keras.layers.BatchNormalization(axis=3)
 
-    def __call__(self, inputs, training=None):
+    def call(self, inputs, training=None):
         skip = inputs
 
         out = self.conv1(inputs, training=training)
@@ -61,7 +60,7 @@ class ConvolutionalBlock(tf.keras.layers.Layer):
         self.convS = tf.keras.layers.Conv2D(filters=f_3, kernel_size=(1, 1), strides=(s, s))
         self.bnS = tf.keras.layers.BatchNormalization(axis=3)
 
-    def __call__(self, inputs, training=None):
+    def call(self, inputs, training=None):
         skip = inputs
 
         out = self.conv1(inputs, training=training)
@@ -85,14 +84,14 @@ class ConvolutionalBlock(tf.keras.layers.Layer):
 
 
 class TinyResNet(tf.keras.Model):
-    stage1: List[Callable]
-    stage2: List[Callable]
-    stage3: List[Callable]
-    stage4: List[Callable]
-    stage5: List[Callable]
-    average_pool: Callable
-    flatten: Callable
-    classification_layer: Callable
+    stage1: List[tf.keras.layers.Layer]
+    stage2: List[tf.keras.layers.Layer]
+    # stage3: List[tf.keras.layers.Layer]
+    # stage4: List[tf.keras.layers.Layer]
+    stage5: List[tf.keras.layers.Layer]
+    average_pool: tf.keras.layers.Layer
+    flatten: tf.keras.layers.Layer
+    classification_layer: tf.keras.layers.Layer
 
     def __init__(self, num_classes=4):
         super(TinyResNet, self).__init__()
@@ -105,42 +104,42 @@ class TinyResNet(tf.keras.Model):
         ]
 
         self.stage2 = [
-            ConvolutionalBlock(f=3, filters=[32, 32, 128], s=1),
-            IdentityBlock(f=3, filters=[32, 32, 128])
+            ConvolutionalBlock(f=3, filters=[16, 16, 64], s=1),
+            IdentityBlock(f=3, filters=[16, 16, 64])
         ]
 
-        self.stage3 = [
-            ConvolutionalBlock(f=3, filters=[64, 64, 256], s=2),
-            IdentityBlock(f=3, filters=[64, 64, 256])
-        ]
-
-        self.stage4 = [
-            ConvolutionalBlock(f=3, filters=[128, 128, 512], s=2),
-            IdentityBlock(f=3, filters=[128, 128, 512])
-        ]
+        # self.stage3 = [
+        #     ConvolutionalBlock(f=3, filters=[64, 64, 256], s=2),
+        #     IdentityBlock(f=3, filters=[64, 64, 256])
+        # ]
+        #
+        # self.stage4 = [
+        #     ConvolutionalBlock(f=3, filters=[128, 128, 512], s=2),
+        #     IdentityBlock(f=3, filters=[128, 128, 512])
+        # ]
 
         self.stage5 = [
-            ConvolutionalBlock(f=3, filters=[256, 256, 1024], s=2),
-            IdentityBlock(f=3, filters=[256, 256, 1024])
+            ConvolutionalBlock(f=3, filters=[32, 32, 128], s=2),
+            IdentityBlock(f=3, filters=[32, 32, 128])
         ]
 
         self.average_pool = tf.keras.layers.AveragePooling2D(pool_size=(2, 2), padding='valid')
         self.flatten = tf.keras.layers.Flatten()
         self.classification_layer = tf.keras.layers.Dense(num_classes)
 
-    def __call__(self, inputs, training=None):
+    def call(self, inputs, training=None):
         out = inputs
         for i in range(len(self.stage1)):
             out = self.stage1[i](out, training=training)
 
         for i in range(len(self.stage2)):
             out = self.stage2[i](out, training=training)
-
-        for i in range(len(self.stage3)):
-            out = self.stage3[i](out, training=training)
-
-        for i in range(len(self.stage4)):
-            out = self.stage4[i](out, training=training)
+        #
+        # for i in range(len(self.stage3)):
+        #     out = self.stage3[i](out, training=training)
+        #
+        # for i in range(len(self.stage4)):
+        #     out = self.stage4[i](out, training=training)
 
         for i in range(len(self.stage5)):
             out = self.stage5[i](out, training=training)
@@ -152,28 +151,6 @@ class TinyResNet(tf.keras.Model):
 
 
 class Training:
-    model: tf.keras.Model
-    train_iterator: Iterator
-    val_iterator: Iterator
-    ds_val = None
-    val_accuracy: tf.keras.metrics.Accuracy
-    traffic_dict = {0: 'back', 1: 'green', 2: 'red', 3: 'yellow'}
-    optimizer = None
-
-    def __init__(self, model, dataset_path: str, batch_size: int, optimizer):
-        # TODO algo failed in this function
-        self.model = model
-
-        builder = tfds.ImageFolder(dataset_path)
-        ds_train = builder.as_dataset(split='train', shuffle_files=True, as_supervised=True)
-        self.ds_val = builder.as_dataset(split='val', shuffle_files=False, as_supervised=True)
-
-        self.train_iterator = iter(ds_train.map(Training.prepare_image).batch(batch_size).repeat())
-        self.val_iterator = iter(self.ds_val.map(Training.prepare_image).batch(batch_size))
-        self.val_accuracy = tf.keras.metrics.Accuracy()
-
-        self.optimizer = optimizer
-
     @staticmethod
     def prepare_image(image: np.ndarray, label: np.ndarray):
         image = tf.cast(image, tf.float32)
@@ -182,96 +159,117 @@ class Training:
         image = tf.image.random_brightness(image, max_delta=0.5, seed=42)
         return image, label
 
+    @staticmethod
     @tf.function
-    def train_step(self, images, labels):
+    def train_step(model, optimizer, images, labels):
         with tf.GradientTape() as tape:
-            predictions = self.model(images, training=True)
+            predictions = model(images, training=True)
             # calculate Loss
             loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels, predictions)
             loss = tf.reduce_mean(loss)
 
         # calculate gradients (backward pass)
-        gradients = tape.gradient(loss, self.model.trainable_variables)
-        self.optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
+        gradients = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, model.trainable_variables))
         return loss
 
-    def validation(self):
-        self.val_accuracy.reset_states()
-        val_iterator = iter(self.val_iterator)
+    @staticmethod
+    def validation(model, ds_val, val_accuracy, batch_size):
+        val_accuracy.reset_states()
+        val_iterator = iter(ds_val.map(Training.prepare_image).batch(batch_size))
         try:
             while True:
-                x, y = next(val_iterator)
-                predictions = self.model(x, training=False)
+                x, y = val_iterator.get_next()
+                predictions = model(x, training=False)
                 predictions = tf.nn.softmax(predictions)
                 predictions = tf.argmax(predictions, axis=-1)
-                self.val_accuracy.update_state(y, predictions)
+                val_accuracy.update_state(y, predictions)
         except tf.errors.OutOfRangeError:
             pass
 
-        return self.val_accuracy.result().numpy()
+        return val_accuracy.result().numpy()
 
-    def train_loop(self, epochs: int, steps_per_epoch: int):
+    @staticmethod
+    def train_loop(model, optim, ds_train: tf.data.Dataset, ds_val: tf.data.Dataset, val_accuracy,
+                   batch_size: int, epochs: int, steps_per_epoch: int):
+
+        train_iterator = iter(ds_train.map(Training.prepare_image).batch(batch_size).repeat())
+
         for epoch in range(epochs):
             losses = []
-            for step in tqdm(range(steps_per_epoch)):
-                x, y = next(self.train_iterator)
-                loss = self.train_step(x, y)
+            for _ in tqdm(range(steps_per_epoch)):
+                x, y = train_iterator.get_next()
+                loss = Training.train_step(model, optim, x, y)
                 losses.append(loss)
 
-            val_acc = self.validation()
+            val_acc = Training.validation(model, ds_val, val_accuracy, batch_size)
 
             print(
                 f'Epoch {epoch + 1}, '
                 f'Training Loss: {tf.reduce_mean(losses).numpy()}, '
                 f'Evaluation Accuracy: {val_acc * 100}')
 
-    def prediction(self):
-        val_ds = self.ds_val.map(Training.prepare_image).batch(1)
-        val_iterator = iter(val_ds)
+    @staticmethod
+    def prediction(model, ds_val: tf.data.Dataset, class_dict: Dict[int, str]):
+        val_iterator = iter(ds_val.map(Training.prepare_image).batch(1))
         try:
             while True:
-                x, y = next(val_iterator)
+                x, y = val_iterator.get_next()
 
-                predict = self.model(x, training=False)
+                predict = model(x, training=False)
                 predict = tf.nn.softmax(predict)
                 predict = tf.argmax(predict, axis=-1)
 
                 if predict != y:
                     plt.imshow(x[0])
                     plt.show()
-                    pred = self.traffic_dict[np.array(predict)[0]]
-                    label = self.traffic_dict[np.array(y)[0]]
+                    pred = class_dict[np.array(predict)[0]]
+                    label = class_dict[np.array(y)[0]]
                     print(f"NN-Prediction: {pred}, Label: {label}")
 
         except tf.errors.OutOfRangeError:
             pass
 
-    def safe_weights(self, path: str):
-        self.model.save_weights(path)
+    @staticmethod
+    def save_weights(model: TinyResNet, path: str):
+        model.save_weights(path)
 
-    def load_weights(self, path: str):
-        # model.build((None, 64, 64, 3))
-        # print(model.summary())
-        self.model.load_weights(path)
+    @staticmethod
+    def load_weights(model: TinyResNet, path: str):
+        model.load_weights(path)
 
 
 if __name__ == '__main__':
-    import tarfile
+    # import tarfile
+    flag_train = True
     path_dataset = './traffic_light_data'
-    path_dataset_tar = path_dataset + '.tar.xz'
+    # path_dataset_tar = path_dataset + '.tar.xz'
+    #
+    # if path_dataset_tar.endswith('.tar.xz'):
+    #     tar = tarfile.open(path_dataset_tar, 'r:xz')
+    #     tar.extractall()
+    #     tar.close()
 
-    if path_dataset_tar.endswith('.tar.xz'):
-        tar = tarfile.open(path_dataset_tar, 'r:xz')
-        tar.extractall()
-        tar.close()
+    builder = tfds.ImageFolder(path_dataset)
+    dataset_train = builder.as_dataset(split='train', shuffle_files=True, as_supervised=True)
+    dataset_val = builder.as_dataset(split='val', shuffle_files=False, as_supervised=True)
+    traffic_dict = {0: 'back', 1: 'green', 2: 'red', 3: 'yellow'}
 
     tiny_resnet = TinyResNet()
-    optim = tf.keras.optimizers.Adam(learning_rate=1e-3)
+    tiny_resnet.build((None, 64, 64, 3))
+    print(tiny_resnet.summary())
+    if flag_train:
+        optimizers = tf.keras.optimizers.Adam(learning_rate=1e-3)
+        validation_accuracy = tf.keras.metrics.Accuracy()
 
-    train_class = Training(tiny_resnet, path_dataset, optim, 32)
-    train_class.train_loop(8, 500)
-    train_class.safe_weights('weights.h5')
-    train_class.prediction()
+        Training.train_loop(tiny_resnet, optimizers, dataset_train, dataset_val, validation_accuracy,
+                            batch_size=32, epochs=16, steps_per_epoch=500)
+
+        Training.save_weights(tiny_resnet, 'weights.h5')
+    else:
+        Training.load_weights(tiny_resnet, 'weights.h5')
+
+    Training.prediction(tiny_resnet, dataset_val, traffic_dict)
 
 
 
