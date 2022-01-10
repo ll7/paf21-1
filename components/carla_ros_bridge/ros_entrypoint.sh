651f8cfd823c0f2ab446992916d07cca9075b3d4
berghojo <berghoff.joshua@googlemail.com>
2022-01-10 20:48:21 +0100

    minor bugfix

--- components/carla_ros_bridge/ros_entrypoint.sh
+++ components/carla_ros_bridge/ros_entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # setup ros environment
 source "/opt/carla-ros-bridge/install/setup.bash"
-rostopic echo /carla/ego_vehicle/ackermann_control/control_info
+
 
 # wait for the CARLA simulator to launch
 sleep $CARLA_SIM_WAIT_SECS
