48bb7e24774a015a2167a3e30a1343bc032334fe
berghojo <berghoff.joshua@googlemail.com>
2022-01-12 16:02:41 +0100

    removed bag files bag files hosted at megastore

--- components/integration-tests-startup.sh
+++ components/integration-tests-startup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 source /opt/ros/noetic/setup.bash
-
+apt-get update && apt-get install -y ros-noetic-ackermann-msgs && rm -rf /var/lib/apt/lists/*
 # make sure all containers are ready
 sleep 10
 
 # replay the recorded ROS messages and dump the output topics to a file
 rostopic echo /carla/ego_vehicle/ackermann_cmd > out.msgs &
-rosbag play /app/recordings/recording.bag &
+rosbag play /app/recordings/recording_0.bag /app/recordings/recording_1.bag /app/recordings/recording_2.bag /app/recordings/recording_3.bag /app/recordings/recording_4.bag /app/recordings/recording_5.bag &
 
 # wait for either of the subtasks to terminate
 wait -n
