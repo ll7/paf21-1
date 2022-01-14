9fd212f2dfb51e1b5e7d8a7e80c32d7f761b4d08
Marco Tröster <marco@troester-gmbh.de>
2022-01-14 16:54:48 +0100

    fix detection issues: - fix cutting patches - fix bgr encoding -> rgb - beautify code

--- components/traffic_light_detection/node/src/traffic_light_detection/detection.py
+++ components/traffic_light_detection/node/src/traffic_light_detection/detection.py
@@ -1,6 +1,6 @@
 """A module that detects traffic lights"""
 
-from typing import Tuple, List
+from typing import Tuple, List, Dict
 import yaml
 
 from cv2 import cv2
@@ -17,31 +17,20 @@ from traffic_light_detection.tld_info import TrafficLightPhase, TrafficLightInfo
 class TrafficLightDetector:
     """A module that detects traffic lights"""
     # pylint: disable=too-few-public-methods
-    model: Model
-    lower_mask: Tuple[int, int, int]
-    upper_mask: Tuple[int, int, int]
-    box_offset: int = 0
-    # enhanced_dim: Tuple[int, int]
-    # crop_left_right: int
-    # crop_top_bottom: int
-    # value_backside: int
 
     def __init__(self, config_path):
         with open(config_path, encoding='utf-8') as file:
             config = yaml.safe_load(file)
-            self.model = load_model(config['weights_path'])
-            self.input_shape = config['nn_input_size']
-            self.classes_dict = config['classes']
-            self.lower_mask = config['lower_bound']
-            self.upper_mask = config['upper_bound']
-            self.box_offset = config['box_offset']
-            # self.enhanced_dim = config['enhanced_dim']
-            # self.value_backside = config['value_backside']
-            # self.crop_top_bottom = config['crop_top_bottom']
-            # self.crop_left_right = config['crop_left_right']
+            self.model: Model = load_model(config['weights_path'])
+            self.input_shape: Tuple[int, int, int] = config['nn_input_size']
+            self.classes_dict: Dict[int, str] = config['classes']
+            self.lower_mask: Tuple[int, int, int] = config['lower_bound']
+            self.upper_mask: Tuple[int, int, int] = config['upper_bound']
+            self.box_offset: int = config['box_offset']
+        self.counter: int = 1
 
     def detect_traffic_light(self, semantic_image: np.ndarray, rgb_image: np.ndarray,
-                             depth_image: np.ndarray) -> TrafficLightInfo:
+                             depth_image: np.ndarray) -> TrafficLightInfo or None:
         """main function to get traffic lights and distance"""
         tl_info = None
         patches = self._find_traffic_light_patches(semantic_image)
@@ -51,24 +40,25 @@ class TrafficLightDetector:
 
             nearest_obj_id = np.argmin(distances)
             dist, patch = distances[nearest_obj_id], patches[nearest_obj_id]
+            scaling_factor = (rgb_image.shape[0] // semantic_image.shape[0], 
+                              rgb_image.shape[1] // semantic_image.shape[1]) 
+            rgb_patch = [patch[0] * scaling_factor[0], patch[1] * scaling_factor[1],
+                         patch[2] * scaling_factor[0], patch[3] * scaling_factor[1]]
 
             if dist < 100:
-                image_patch = TrafficLightDetector._cut_image_patch(patch, rgb_image)
-                tl_phase = self._classify_tl_phase(image_patch)
+                cut_rgb_image = TrafficLightDetector._cut_image_patch(rgb_patch, rgb_image)
+                tl_phase = self._classify_tl_phase(cut_rgb_image)
                 tl_info = TrafficLightInfo(phase=tl_phase, distance=dist)
 
-                bottom_left = (patch[0], patch[1])
-                top_right = (patch[0] + patch[2], patch[1] + patch[3])
-                log_image = cv2.rectangle(np.array(rgb_image), bottom_left, top_right,
-                                          color=(0, 0, 255), thickness=1)
-                log_image = TrafficLightDetector._print_text_to_image(dist, tl_phase, log_image)
-
-                # TODO: add logging logic here ...
+                if self.counter % 10 == 0 and self.counter < 10000:
+                    log_image = TrafficLightDetector._print_text_to_image(
+                        dist, tl_phase, rgb_image, rgb_patch)
+                    cv2.imwrite(f'/app/logs/test_data_tl_{self.counter}.png', log_image)
+                self.counter += 1
 
         return tl_info
 
     def _find_traffic_light_patches(self, orig_image: np.ndarray):
-        """gets bounding boxes around traffic lights (analog applicable on other objects)"""
         lower_mask = np.array(self.lower_mask)
         upper_mask = np.array(self.upper_mask)
         masked_image = cv2.inRange(orig_image, lower_mask, upper_mask)
@@ -86,14 +76,12 @@ class TrafficLightDetector:
 
     @staticmethod
     def _cut_image_patch(patch: List[int], image: np.ndarray):
-        """gets bounding boxes around traffic lights (analog applicable on other objects)
-        and returns zoomed in image of the traffic light"""
         x_1, y_1, x_2, y_2 = TrafficLightDetector._get_corners(patch, image.shape)
-        return image[y_1:y_2, x_1:x_2, :]
+        cut_img = image[y_1:y_2, x_1:x_2, :]
+        return cut_img
 
     @staticmethod
-    def _print_text_to_image(meters, tl_color, visualize_image):
-        """adds text to an image"""
+    def _print_text_to_image(meters, tl_color, image, patch):
         font = cv2.FONT_HERSHEY_SIMPLEX
         text_pos = (10, 500)
         font_scale = 1
@@ -101,8 +89,13 @@ class TrafficLightDetector:
         thickness = 1
         line_type = 2
 
+        bottom_left = (patch[0], patch[1])
+        top_right = (patch[0] + patch[2], patch[1] + patch[3])
+        image = cv2.rectangle(np.array(image), bottom_left, top_right,
+                              color=(0, 0, 255), thickness=1)
+
         message = f'Distance: {meters} m, Color:{tl_color}'
-        return cv2.putText(np.array(visualize_image), message,
+        return cv2.putText(np.array(image), message,
                            text_pos, font, font_scale, font_color,
                            thickness, line_type)
 
@@ -114,9 +107,10 @@ class TrafficLightDetector:
         y2_cord = min(max(y1_cord + rect[3], y1_cord + 1), img_shape[0])
         return [x1_cord, y1_cord, x2_cord, y2_cord]
 
-    def _classify_tl_phase(self, image: np.ndarray) -> TrafficLightPhase:
+    def _classify_tl_phase(self, image_bgr: np.ndarray) -> TrafficLightPhase:
+        image_rgb = image_bgr[:, :, ::-1]
         resize_shape = (self.input_shape[0], self.input_shape[1])
-        image_preproc = resize_image(image, resize_shape)
+        image_preproc = resize_image(image_rgb, resize_shape)
         pred = np.squeeze(self.model(np.expand_dims(image_preproc, axis=0)).numpy())
         class_id = int(np.argmax(pred))
         return TrafficLightPhase(class_id)
