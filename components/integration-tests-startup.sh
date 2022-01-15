9e843935f80815fc603f3d750616542391fa1d63
Marco Tröster <marco@troester-gmbh.de>
2022-01-15 01:31:39 +0100

    51 fix roslaunch no error on node failure (#55)
    * add logic to detect ros node crashes during tests
    
    * fixed typo and beautify
    
    * fix typo

--- components/integration-tests-startup.sh
+++ components/integration-tests-startup.sh
@@ -5,6 +5,9 @@ apt-get update && apt-get install -y ros-noetic-ackermann-msgs && rm -rf /var/li
 # make sure all containers are ready
 sleep 10
 
+# list summary of available nodes before the test
+nodes_before=$(rosnode list)
+
 # replay the recorded ROS messages and dump the output topics to a file
 rostopic echo /carla/ego_vehicle/ackermann_cmd > out.msgs &
 rosbag play /app/recordings/recording_0.bag /app/recordings/recording_1.bag /app/recordings/recording_2.bag /app/recordings/recording_3.bag /app/recordings/recording_4.bag /app/recordings/recording_5.bag &
@@ -12,6 +15,17 @@ rosbag play /app/recordings/recording_0.bag /app/recordings/recording_1.bag /app
 # wait for either of the subtasks to terminate
 wait -n
 kill $(jobs -p)
+
+# list summary of available nodes after the test
+nodes_after=$(rosnode list)
+
+# assert that all nodes are still available after the test
+# info: this is required because roslaunch always exits with 0 code although
+#       nodes were crashing, so a crash can only be detected by comparing the lists
+if [ $nodes_before -ne $nodes_after ]; then
+    echo 'test failed! some nodes crashed during test!'
+    exit 1
+fi
 
 # assert that the dumped output actually contains ROS messages
 if [[ ! $(cat out.msgs) ]]; then
