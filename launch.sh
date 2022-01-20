e88087335ba18bcfd099f7aadab03e04e12d0265
Marco Tröster <marco@troester-gmbh.de>
2022-01-21 00:06:10 +0100

    fail on build error
    Co-authored-by: Axel Böll <axelboell@web.de>

--- launch.sh
+++ launch.sh
@@ -3,8 +3,10 @@
 SCENARIO=default_scenario
 
 pushd components
-    docker-compose -f driving-components-build.yml build
     docker-compose -f carla-sim-build.yml build
+    if [ $? -ne 0 ]; then exit 1; fi
+    docker-compose -f driving-components-build.yml build
+    if [ $? -ne 0 ]; then exit 1; fi
 popd
 
 pushd scenarios/$SCENARIO
