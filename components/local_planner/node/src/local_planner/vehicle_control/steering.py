e2e3c19337157c3e0058fcedc89157f484f581bf
Johannes Stoljar <johannes.stoljar@uni-a.de>
2022-03-17 00:22:12 +0100

    Increase min distance for steering

--- components/local_planner/node/src/local_planner/vehicle_control/steering.py
+++ components/local_planner/node/src/local_planner/vehicle_control/steering.py
@@ -36,13 +36,13 @@ class NaiveSteeringController:
 
     @property
     def min_dist_ahead(self) -> float:
-        speed= self.vehicle.velocity_mps
+        speed = self.vehicle.velocity_mps
         if speed < 45.0 / 3.6:
-            return 5.0
+            return 6.0
         elif speed < 55.0 / 3.6:
-            return 7.0
+            return 8.0
         elif speed < 65.0 / 3.6:
-            return 14.0
+            return 15.0
         elif speed < 75.0 / 3.6:
             return 17.0
         return 22.0
