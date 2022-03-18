b4ec4cef26c665925f25a09589ccc2c3aa729d88
Marco Tröster <marco@troester-gmbh.de>
2022-03-18 21:21:24 +0100

    integrate additional competition yaml infos

--- components/carla_ros_bridge/ros_entrypoint.sh
+++ components/carla_ros_bridge/ros_entrypoint.sh
@@ -7,16 +7,14 @@ updatedb
 export CONFIG_FILE_FULL_PATH="$(locate "$CONFIG_FILE")"
 
 echo "config file: $CONFIG_FILE_FULL_PATH"
-export SPAWN_POINT="$(python /scripts/parse_spawn_pos.py $CONFIG_FILE_FULL_PATH)"
+export TOWN="$(python /scripts/load_env_config.py $CONFIG_FILE_FULL_PATH --town)"
+export SPAWN_POINT="$(python /scripts/load_env_config.py $CONFIG_FILE_FULL_PATH --spawn-pos)"
+export NUM_CARS="$(python /scripts/load_env_config.py $CONFIG_FILE_FULL_PATH --num-cars)"
+export NUM_PEDESTRIANS="$(python /scripts/load_env_config.py $CONFIG_FILE_FULL_PATH --num-peds)"
+export RAND_SPAWN_SEED="$(python /scripts/load_env_config.py $CONFIG_FILE_FULL_PATH --spawn-seed)"
 
-# TODO: parse following settings from the YAML config (don't hardcode this here ...)
-export TOWN="${CONFIG_FILE_FULL_PATH:17:6}"
-export NUM_CARS=0
-export NUM_PEDESTRIANS=0
-
-echo "spawn pos: $SPAWN_POINT"
-echo "town: $TOWN"
-echo "cars: $NUM_CARS, pedestrians: $NUM_PEDESTRIANS"
+echo "spawn pos: $SPAWN_POINT in town $TOWN"
+echo "cars: $NUM_CARS, pedestrians: $NUM_PEDESTRIANS, seed: $RAND_SPAWN_SEED"
 
 # load the configuration for the competition manager
 sleep 2
@@ -28,5 +26,6 @@ sleep $CARLA_SIM_WAIT_SECS
 # launch the given ROS nodes from Docker CMD args
 roslaunch $@ &
 sleep 5 && python /opt/carla/PythonAPI/examples/spawn_npc.py \
-    --host carla-simulator -n $NUM_CARS -w $NUM_PEDESTRIANS --safe --sync &
+    --host carla-simulator --safe --sync \
+    -n $NUM_CARS -w $NUM_PEDESTRIANS -s $RAND_SPAWN_SEED &
 wait
