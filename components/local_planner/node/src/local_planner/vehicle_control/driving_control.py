1cf4265750dea14594db13a9954e92c1e10ccd12
Johannes Stoljar <johannes.stoljar@uni-a.de>
2022-03-21 21:38:58 +0100

    Beautify code

--- components/local_planner/node/src/local_planner/vehicle_control/driving_control.py
+++ components/local_planner/node/src/local_planner/vehicle_control/driving_control.py
@@ -53,8 +53,8 @@ class DrivingController:  # pylint: disable=too-many-instance-attributes
         vehicle follow the suggested ideal route"""
         # generate logs for each driving signal tick
         steering_angle = self.steer_control.compute_steering_angle(self.route_waypoints)
-        targetspeed =  self.target_velocity_mps # if self.route_waypoints else 0.0
-        signal = DrivingSignal(steering_angle, targetspeed)
+        target_speed = self.target_velocity_mps  # if self.route_waypoints else 0.0
+        signal = DrivingSignal(steering_angle, target_speed)
         # if self.vehicle.is_ready:
         #     print("Signal. Time : {}, pos[1]: {}, orientation_rad: {}, \
         #         velocity: {}, targetspeed: {}, steering_angle: {}".
