4266bf1040b0c2b590b1f5f614f9cb110f6df235
Marco Tröster <marco@troester-gmbh.de>
2022-03-29 23:00:10 +0200

    add CARLA simulator download / setup script

---
+++ setup_simulator.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+if [ ! -d "./simulator" ]; then
+    echo 'downloading the CARLA simulator ...'
+    CARLA_SIM_URL=https://carla-releases.s3.eu-west-3.amazonaws.com/Linux/CARLA_0.9.10.1.tar.gz
+    mkdir ./simulator
+    wget -q -O- $CARLA_SIM_URL | tar xzf - -C ./simulator
+    echo 'download successful!'
+
+    ADD_ASSETS_URL=https://carla-releases.s3.eu-west-3.amazonaws.com/Linux/AdditionalMaps_0.9.10.tar.gz
+    cd simulator/Import && wget -q $ADD_ASSETS_URL && cd ..
+    echo 'download successful!'
+    echo 'importing additional assets ...'
+    ./ImportAssets.sh > /dev/null 2>&1 && rm -rf Import/*.tar.gz
+    echo 'import successful!'
+fi
+
+echo 'simulator is ready for use'
