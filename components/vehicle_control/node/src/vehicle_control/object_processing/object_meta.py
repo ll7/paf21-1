87b269ea74f3a1a96a9825e64999b33e16f34060
Johannes Stoljar <johannes.stoljar@uni-a.de>
2022-03-29 22:32:31 +0200

    Set max number of object meta entries to 5

--- components/vehicle_control/node/src/vehicle_control/object_processing/object_meta.py
+++ components/vehicle_control/node/src/vehicle_control/object_processing/object_meta.py
@@ -13,14 +13,14 @@ class ObjectMeta:
     trajectory: List[Tuple[float, float]]
     velocity: float = 0.0
     max_vel_change_rate = 2.0
-    max_trajectory_entries: int = 10
+    max_trajectory_entries: int = 5
 
     def update_object_meta(self, point: Tuple[float, float], delta_time: float):
         """Update the object meta with a new position"""
         last_position = self.trajectory[-1]
         self.trajectory.append(point)
-        if len(self.trajectory) > 10:
-            self.trajectory = self.trajectory[-10:]
+        if len(self.trajectory) > self.max_trajectory_entries:
+            self.trajectory = self.trajectory[-self.max_trajectory_entries:]
 
         velocity = dist(point, last_position) / delta_time
         self.velocity = min(velocity, self.velocity + self.max_vel_change_rate)
